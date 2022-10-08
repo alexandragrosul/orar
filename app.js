98fb0368e9448ce7640119dc5e7f7fe4e0882449
Alexandra Grosul <grosulalexandra@gmail.com>
2022-10-08 20:38:21 +0300

    addit bootstrap

--- app.js
+++ app.js
@@ -89,32 +89,148 @@ const orar = {
     ],
 };
 
+const lessonsTime = [{
+        start: '8:30',
+        end: '09:15'
+    },
+    {
+        start: '9:30',
+        end: '10:20'
+    },
+    {
+        start: '10:35',
+        end: '11:20'
+    },
+    {
+        start: '11:35',
+        end: '12:20'
+    },
+    {
+        start: '12:30',
+        end: '13:15'
+    },
+    {
+        start: '13:30',
+        end: '14:15'
+    },
+    {
+        start: '14:25',
+        end: '15:10'
+    }
+]
+
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
 const list = document.getElementById('orar_list')
-const day = document.querySelector('.day')
+const dayToDisplay = document.getElementById('day')
+const luniButton = document.getElementById('luni')
+const martiButton = document.getElementById('marti')
+const miercuriButton = document.getElementById('miercuri')
+const joiButton = document.getElementById('joi')
+const vineriButton = document.getElementById('vineri')
 
 const isWeekends = () => {
     return currentDay === 0 || currentDay === 6 ? true : false
 }
 
-const buildOrar = (day, isWeekend) => {
-    if (isWeekend) {
-        for (const predmet of orar[1]) {
-            const newLi = document.createElement('li')
-            newLi.innerText = predmet.name
-            list.appendChild(newLi)
-
-        }
-    } else {
-        for (const predmet of orar[++day]) {
-            const newLi = document.createElement('li')
-            newLi.innerText = predmet.name
-            list.appendChild(newLi)
-
-        }
+const getDay = (day) => {
+    switch (day) {
+        case 1:
+            return 'Luni'
+        case 2:
+            return 'Marti'
+        case 3:
+            return 'Miercuri'
+        case 4:
+            return 'Joi'
+        case 5:
+            return 'Vineri'
+        default:
+            return "Weekenduri"
     }
 }
+
+const buildOrar = (day, isWeekend) => {
+    list.innerHTML = ''
+    if (isWeekend) {
+        luniButton.classList.add('active')
+        for (const predmet of orar[1]) {
+            const newLi = document.createElement('li')
+            newLi.classList.add('list-group-item')
+            newLi.innerText = predmet.name
+            list.appendChild(newLi)
+
+        }
+        // dayToDisplay.innerText = `Orar pentru Luni`
+    } else {
+        switch (day) {
+            case 1:
+                luniButton.classList.add('active')
+                break
+            case 2:
+                martiButton.classList.add('active')
+                break
+            case 3:
+                miercuriButton.classList.add('active')
+                break
+            case 4:
+                joiButton.classList.add('active')
+                break
+            case 5:
+                vineriButton.classList.add('active')
+                break
+
+
+        }
+        for (const predmet of orar[day]) {
+            const newLi = document.createElement('li')
+            newLi.classList.add('list-group-item')
+            newLi.innerText = predmet.name
+            list.appendChild(newLi)
+
+        }
+        // dayToDisplay.innerText = `Orar pentru ${getDay(currentDay+1)}`
+    }
+}
+
+const removeActiveTab = () => {
+    luniButton.classList.remove('active')
+    martiButton.classList.remove('active')
+    miercuriButton.classList.remove('active')
+    joiButton.classList.remove('active')
+    vineriButton.classList.remove('active')
+}
+
+luniButton.addEventListener('click', () => {
+    removeActiveTab()
+
+    luniButton.classList.add('active')
+
+    buildOrar(1, false)
+})
+martiButton.addEventListener('click', () => {
+    removeActiveTab()
+    martiButton.classList.add('active')
+    buildOrar(2, false)
+})
+miercuriButton.addEventListener('click', () => {
+    removeActiveTab()
+
+    miercuriButton.classList.add('active')
+    buildOrar(3, false)
+})
+joiButton.addEventListener('click', () => {
+    removeActiveTab()
+
+    joiButton.classList.add('active')
+    buildOrar(4, false)
+})
+vineriButton.addEventListener('click', () => {
+    removeActiveTab()
+
+    vineriButton.classList.add('active')
+    buildOrar(5, false)
+})
 
 buildOrar(currentDay, isWeekends())
